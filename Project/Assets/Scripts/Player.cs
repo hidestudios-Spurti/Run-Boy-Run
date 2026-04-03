b57832eea85764cf88324c3ce28dfcb5aece5c5b
hidestudios-Spurti <hidestudios26@gmail.com>
2026-04-03 13:46:04 +0200

    Project V3.1
    fixed the bosst and it now lasts 5s

--- Project/Assets/Scripts/Player.cs
+++ Project/Assets/Scripts/Player.cs
@@ -1,18 +1,21 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class Player : MonoBehaviour
 {
     [Header("Movement Settings")]
     public Rigidbody2D rb;
     public float _currentSpeed = 0f;
-    public float _maxSpeed = 8f;
-    public float _boostSpeed = 10f;
-    public float _acceleration = 4f;
+    public float _maxSpeed = 10f;
+    public float _boostSpeed = 15;
+    public float _acceleration = 5f;
     public float _deceleration = 10f;
-    public float _turnDeceleration = 20f;
+    public float _turnDeceleration = 40f;
     public float _jumpStrenght = 9f;
     public bool _isGrounded = false;
-
+    public float _boostEndTime = 0f;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -25,28 +28,20 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
         {
             rb.AddForceY(_jumpStrenght, ForceMode2D.Impulse);
-        }
-        if (Input.GetKeyDown(KeyCode.LeftShift) && _currentSpeed >= 0f)
-        {
-
-            _currentSpeed = _boostSpeed;
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftShift)){ 
-          
-            _currentSpeed = -_boostSpeed; }
-        ;
+        }   
+        
     }
 
 
 
-            private void FixedUpdate()
+    private void FixedUpdate()
             {
                 Movement();
             }
 
     void Movement()
     {
-        float horizontal = Input.GetAxis("Horizontal");
+         float horizontal = Input.GetAxis("Horizontal");
         bool pressingOpposite = Mathf.Sign(horizontal) != Mathf.Sign(_currentSpeed) && horizontal != 0 && Mathf.Abs(_currentSpeed) > 0.1f;
 
         if (pressingOpposite)
@@ -66,7 +61,31 @@ public class Player : MonoBehaviour
             _currentSpeed = Mathf.MoveTowards(_currentSpeed, 0f, _deceleration *  Time.fixedDeltaTime);
         }
 
+        if (Input.GetKey(KeyCode.LeftShift) && horizontal > 0)
+        {
+            _boostSpeed = 15f;
+            _maxSpeed = 15f;
+            _currentSpeed = _boostSpeed;
+            _boostEndTime = Time.time + 5f;
+            
+        }
+        
+        if (Input.GetKey(KeyCode.LeftShift) && horizontal < 0 )
+        {
+            _boostSpeed = 15f;
+            _maxSpeed = 15f;
+            _currentSpeed = -_boostSpeed;
+            _boostEndTime = Time.time + 5f;
+            
+        }
+
+        if ( Time.time >= _boostEndTime)
+        {
+            _maxSpeed = 10f;
+        }
+        
         rb.linearVelocity = new Vector2(_currentSpeed, rb.linearVelocity.y);
+       
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
